4818f4478c126accea013d3ba3f4ebca0d362f79
Robert Seifert <rseifert.phone@gmail.com>
2017-03-13 02:05:20 -0400

    Moved files around to improve navigation and to place same files near each other

---
+++ src/main/scala/com/builtbroken/atomic/content/accelerator/ItemDarkMatter.java
@@ -0,0 +1,12 @@
+package com.builtbroken.atomic.content.accelerator;
+
+import com.builtbroken.atomic.content.items.ItemCell;
+
+/** Strange matter cell */
+public class ItemDarkMatter extends ItemCell
+{
+    public ItemDarkMatter(int itemID)
+    {
+        super(itemID);
+    }
+}
