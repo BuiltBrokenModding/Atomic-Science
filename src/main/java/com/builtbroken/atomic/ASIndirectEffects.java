53ae404f1c2c84db688a0ada6d2e6f853191483d
Robert WC Seifert <rseifert.phone@gmail.com>
2018-04-20 02:18:07 -0400

    Started on tracking living updates to apply damage

--- src/main/java/com/builtbroken/atomic/ASIndirectEffects.java
+++ src/main/java/com/builtbroken/atomic/ASIndirectEffects.java
@@ -4,7 +4,12 @@ import com.builtbroken.atomic.api.AtomicScienceAPI;
 import com.builtbroken.atomic.api.effect.IIndirectEffectSource;
 import com.builtbroken.atomic.config.ConfigRadiation;
 import com.builtbroken.atomic.content.effects.IndirectEffectType;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.player.EntityInteractEvent;
 
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
@@ -12,10 +17,15 @@ import net.minecraft.entity.Entity;
  */
 public class ASIndirectEffects
 {
+    //Radiation is based on -> https://en.wikipedia.org/wiki/Rad_(unit)
     public static final String NBT_RADS = AtomicScience.PREFIX + "rads";
+    public static final String NBT_RADS_ADD = AtomicScience.PREFIX + "rads_add_time";
+    public static final String NBT_RADS_REMOVE = AtomicScience.PREFIX + "rads_remove_time";
 
     public static void register()
     {
+        MinecraftForge.EVENT_BUS.register(new ASIndirectEffects());
+
         AtomicScienceAPI.RADIATION = new IndirectEffectType("radiation")
         {
             @Override
@@ -24,8 +34,28 @@ public class ASIndirectEffects
                 float rads = target.getEntityData().getFloat(NBT_RADS);
                 rads += power;
                 target.getEntityData().setFloat(NBT_RADS, Math.max(0, Math.min(ConfigRadiation.MAX_RADS, rads)));
+                target.getEntityData().setLong(NBT_RADS_ADD, System.currentTimeMillis());
+                //TODO fire events
+                //TODO sync to client
             }
         };
+    }
+
+    @SubscribeEvent
+    public void onEntityUpdate(LivingEvent.LivingUpdateEvent event)
+    {
+        EntityLivingBase entity = event.entityLiving;
+        if (entity.getEntityData().hasKey(NBT_RADS))
+        {
+            //TODO slowly decrease
+            //TODO if over a set amount do damage
+        }
+    }
+
+    //@SubscribeEvent
+    public void onInteractEntity(EntityInteractEvent entityInteractEvent)
+    {
+        //TODO use to allow inserting items into inventories
     }
 
     //TODO track entities and tiles
