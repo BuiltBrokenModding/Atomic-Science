8a07ca6e681c20ac9c3bb74681c558e430167519
Robert WC Seifert <rseifert.phone@gmail.com>
2018-04-21 23:40:34 -0400

    Created events for indirect effects

---
+++ src/main/java/com/builtbroken/atomic/content/effects/events/IndirectEffectEntityEvent.java
@@ -0,0 +1,45 @@
+package com.builtbroken.atomic.content.effects.events;
+
+import com.builtbroken.atomic.api.effect.IIndirectEffectSource;
+import com.builtbroken.atomic.api.effect.IIndirectEffectType;
+import cpw.mods.fml.common.eventhandler.Cancelable;
+import net.minecraft.entity.Entity;
+
+/**
+ * Radiation event involving an entity
+ *
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 4/21/2018.
+ */
+public class IndirectEffectEntityEvent extends IndirectEffectEvent
+{
+    /** Entity being effected by the radiation */
+    public final Entity target;
+
+    public IndirectEffectEntityEvent(IIndirectEffectSource source, IIndirectEffectType type, Entity target, float power)
+    {
+        super(source, type, power);
+        this.target = target;
+    }
+
+    /**
+     * Called before radiation is applied to an entity. Can be used to change the applied
+     * power value or to cancel the action.
+     * <p>
+     * Is {@link Cancelable}
+     * <p>
+     * Fired on {@link net.minecraftforge.common.MinecraftForge#EVENT_BUS}
+     */
+    @Cancelable
+    public static class Pre extends IndirectEffectEntityEvent
+    {
+        /** Applied power value, for radiation this will be REMs */
+        public float appliedPower;
+
+        public Pre(IIndirectEffectSource source, IIndirectEffectType type, Entity target, float power, float appliedPower)
+        {
+            super(source, type, target, power);
+            this.appliedPower = appliedPower;
+        }
+    }
+}
---
+++ src/main/java/com/builtbroken/atomic/content/effects/events/IndirectEffectEvent.java
@@ -0,0 +1,30 @@
+package com.builtbroken.atomic.content.effects.events;
+
+import com.builtbroken.atomic.api.effect.IIndirectEffectSource;
+import com.builtbroken.atomic.api.effect.IIndirectEffectType;
+import cpw.mods.fml.common.eventhandler.Event;
+
+/**
+ * Base type for all indirect effects (radiation)
+ * <p>
+ * Currently set as internal while events are being developed. Do not use until they are in the API package.
+ *
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 4/21/2018.
+ */
+public class IndirectEffectEvent extends Event
+{
+    /** Source of the radiation */
+    public final IIndirectEffectSource source;
+    /** Type of effect */
+    public final IIndirectEffectType type;
+    /** Power level of the radiation */
+    public final float power;
+
+    public IndirectEffectEvent(IIndirectEffectSource source, IIndirectEffectType type, float power)
+    {
+        this.source = source;
+        this.type = type;
+        this.power = power;
+    }
+}
