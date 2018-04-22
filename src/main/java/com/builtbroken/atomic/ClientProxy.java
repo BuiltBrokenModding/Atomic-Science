8e29e86f4ca97dee5a42982ee7b4012477683ab7
Robert WC Seifert <rseifert.phone@gmail.com>
2018-04-21 23:40:21 -0400

    Created packet to sync rad value to client for display

--- src/main/java/com/builtbroken/atomic/ClientProxy.java
+++ src/main/java/com/builtbroken/atomic/ClientProxy.java
@@ -4,6 +4,11 @@ import com.builtbroken.atomic.content.ASClientReg;
 
 public class ClientProxy extends CommonProxy
 {
+    /** Client Cache: Amount of rads in the environment were the player is standing */
+    public static float RAD_EXPOSURE = 0;
+    /** Client Cache: Amount of rads the player has taken */
+    public static float RAD_PLAYER = 0;
+
     @Override
     public void preInit()
     {
---
+++ src/main/java/com/builtbroken/atomic/lib/network/message/MessageRadiation.java
@@ -0,0 +1,51 @@
+package com.builtbroken.atomic.lib.network.message;
+
+import com.builtbroken.atomic.ClientProxy;
+import com.builtbroken.atomic.lib.network.IPacket;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import net.minecraft.entity.player.EntityPlayer;
+
+/**
+ * Syncs radiation data for the player
+ *
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 4/21/2018.
+ */
+public class MessageRadiation implements IPacket<MessageRadiation>
+{
+    public float rads;
+    public float rads_area;
+
+    public MessageRadiation()
+    {
+        //Empty for default packet handling
+    }
+
+    public MessageRadiation(float rads, float rads_area)
+    {
+        this.rads = rads;
+        this.rads_area = rads_area;
+    }
+
+    @Override
+    public void encodeInto(ChannelHandlerContext ctx, ByteBuf buffer)
+    {
+        buffer.writeFloat(rads);
+        buffer.writeFloat(rads_area);
+    }
+
+    @Override
+    public void decodeInto(ChannelHandlerContext ctx, ByteBuf buffer)
+    {
+        rads = buffer.readFloat();
+        rads_area = buffer.readFloat();
+    }
+
+    @Override
+    public void handleClientSide(EntityPlayer player)
+    {
+        ClientProxy.RAD_EXPOSURE = rads_area;
+        ClientProxy.RAD_PLAYER = rads;
+    }
+}
