7c2254f725b3dd2ee14338218b55e595e23c4029
Robert WC Seifert <rseifert.phone@gmail.com>
2018-05-08 02:37:45 -0400

    Implemented network helpers in TileMachine

--- src/main/java/com/builtbroken/atomic/content/tiles/TileEntityMachine.java
+++ src/main/java/com/builtbroken/atomic/content/tiles/TileEntityMachine.java
@@ -1,38 +1,138 @@
 package com.builtbroken.atomic.content.tiles;
 
+import com.builtbroken.atomic.lib.network.IPacket;
+import com.builtbroken.atomic.lib.network.IPacketIDReceiver;
+import com.builtbroken.atomic.lib.network.netty.PacketSystem;
+import com.builtbroken.atomic.lib.network.packet.PacketTile;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 5/7/2018.
  */
-public class TileEntityMachine extends TileEntity
+public abstract class TileEntityMachine extends TileEntity implements IPacketIDReceiver
 {
-    private int ticks = 0;
+    public static final int DESC_PACKET_ID = -1;
+
+    private int _ticks = 0;
+    private boolean _syncClientNextTick = true;
+
+    //-----------------------------------------------
+    //--------- Update methods ----------------------
+    //-----------------------------------------------
 
     public final void updateEntity()
     {
-        if (ticks == 0)
+        if (_ticks == 0)
         {
             firstTick();
         }
-        update(ticks);
-        ticks++;
-        if (ticks + 1 == Integer.MAX_VALUE)
+        update(_ticks);
+        _ticks++;
+        if (_ticks + 1 == Integer.MAX_VALUE)
         {
-            ticks = 1;
+            _ticks = 1;
+        }
+
+        if(_syncClientNextTick)
+        {
+            _syncClientNextTick = false;
+            sendDescPacket();
         }
     }
 
+    /**
+     * Called on the very fist update loop call.
+     * Only called once after the tile has been created
+     */
     protected void firstTick()
     {
 
     }
 
+    /**
+     * Called each update loop
+     *
+     * @param ticks - number of ticks of the world (20 ticks a second)
+     */
     protected void update(int ticks)
     {
 
     }
+
+    //-----------------------------------------------
+    //--------- Network -----------------------------
+    //-----------------------------------------------
+
+
+    @Override
+    public boolean read(ByteBuf buf, int id, EntityPlayer player, IPacket type)
+    {
+        if (isClient() && id == DESC_PACKET_ID)
+        {
+            readDescPacket(buf, player);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Will send a description packet to the client next tick
+     */
+    protected void syncClientNextTick()
+    {
+        _syncClientNextTick = true;
+    }
+
+    /**
+     * Sends the description packet to the client
+     */
+    protected void sendDescPacket()
+    {
+        PacketTile packetTile = new PacketTile("reactor_desc", DESC_PACKET_ID, this);
+
+        //Collect data
+        List<Object> list = new ArrayList();
+        writeDescPacket(list, null); //TODO get list of players, then send
+        packetTile.addData(list);
+
+        PacketSystem.INSTANCE.sendToAllAround(packetTile, this);
+    }
+
+    /**
+     * Called to write data to the packet
+     * <p>
+     * This works by building a list of objects to write. These objects
+     * will be written when the packet is encoded. This helps reduce issues
+     * trying to encode a bytebuf into a bytebuf.
+     *
+     * @param dataList - list of objects to write
+     * @param player   - player to send the packet, can be null
+     */
+    protected void writeDescPacket(List<Object> dataList, EntityPlayer player)
+    {
+
+    }
+
+    /**
+     * Called to read the packet
+     *
+     * @param buf    - raw data
+     * @param player - player reading the data
+     */
+    protected void readDescPacket(ByteBuf buf, EntityPlayer player)
+    {
+
+    }
+
+    //-----------------------------------------------
+    //--------- Helpers -----------------------------
+    //-----------------------------------------------
 
     protected boolean isServer()
     {
