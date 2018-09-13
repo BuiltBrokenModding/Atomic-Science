e25f6968fffa49c81abad8c135f2e6fe1d61b4bd
Robert WC Seifert <rseifert.phone@gmail.com>
2018-09-13 09:05:58 -0400

    Added items for blocks

--- src/main/java/com/builtbroken/atomic/content/ASItems.java
+++ src/main/java/com/builtbroken/atomic/content/ASItems.java
@@ -12,14 +12,17 @@ import com.builtbroken.atomic.content.items.cell.CreativeTabCells;
 import com.builtbroken.atomic.content.items.cell.ItemFluidCell;
 import com.builtbroken.atomic.content.items.cell.ItemPoweredCell;
 import com.builtbroken.atomic.content.items.wrench.ItemWrench;
+import net.minecraft.block.Block;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.registries.IForgeRegistry;
 
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
@@ -57,11 +60,23 @@ public final class ASItems
     public static ItemWrench itemWrench;
 
 
+    public static ItemBlock blockReactorCell;
+    public static ItemBlock blockReactorController;
+    public static ItemBlock blockSteamFunnel;
+    public static ItemBlock blockSteamTurbine;
+
+    public static ItemBlock blockUraniumOre;
+
+    public static ItemBlock blockChemExtractor;
+    public static ItemBlock blockChemBoiler;
+    public static ItemBlock blockChemCentrifuge;
+
+
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event)
     {
         //Armor
-        ItemHazmat.hazmatArmorMaterial = EnumHelper.addArmorMaterial("HAZMAT", "HAZMAT",5, new int[]{0, 0, 0, 0}, 0, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F);
+        ItemHazmat.hazmatArmorMaterial = EnumHelper.addArmorMaterial("HAZMAT", "HAZMAT", 5, new int[]{0, 0, 0, 0}, 0, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F);
         event.getRegistry().register(itemArmorHazmatHelm = new ItemHazmat(EntityEquipmentSlot.HEAD, "mask"));
         event.getRegistry().register(itemArmorHazmatChest = new ItemHazmat(EntityEquipmentSlot.CHEST, "body"));
         event.getRegistry().register(itemArmorHazmatLegs = new ItemHazmat(EntityEquipmentSlot.LEGS, "leggings"));
@@ -73,12 +88,12 @@ public final class ASItems
 
         //Machine inputs
         event.getRegistry().register(itemFissileFuelCell = new ItemFuelRod("cell.fuel.fissile",
-                        ConfigPower.FUEL_ROD_RUNTIME, ConfigRadiation.RADIOACTIVE_MAT_VALUE_FUEL_ROD
-                        , ConfigRadiation.RADIOACTIVE_REACTOR_VALUE_FUEL_ROD, ConfigThermal.HEAT_REACTOR_FUEL_ROD)
+                ConfigPower.FUEL_ROD_RUNTIME, ConfigRadiation.RADIOACTIVE_MAT_VALUE_FUEL_ROD
+                , ConfigRadiation.RADIOACTIVE_REACTOR_VALUE_FUEL_ROD, ConfigThermal.HEAT_REACTOR_FUEL_ROD)
                 .setRegistryName(AtomicScience.PREFIX + "fissile_fuel_cell"));
         event.getRegistry().register(itemBreederFuelCell = new ItemFuelRod("cell.fuel.breeder",
-                        ConfigPower.BREEDER_ROD_RUNTIME, ConfigRadiation.RADIOACTIVE_MAT_VALUE_BREEDER_ROD
-                        , ConfigRadiation.RADIOACTIVE_REACTOR_VALUE_BREEDER_ROD, ConfigThermal.HEAT_REACTOR_BREEDER_ROD)
+                ConfigPower.BREEDER_ROD_RUNTIME, ConfigRadiation.RADIOACTIVE_MAT_VALUE_BREEDER_ROD
+                , ConfigRadiation.RADIOACTIVE_REACTOR_VALUE_BREEDER_ROD, ConfigThermal.HEAT_REACTOR_BREEDER_ROD)
                 .setRegistryName(AtomicScience.PREFIX + "breeder_fuel_cell"));
 
         //Crafting items
@@ -88,20 +103,40 @@ public final class ASItems
                 .setRegistryName(AtomicScience.PREFIX + "cell_empty"));
 
         event.getRegistry().register(itemYellowCake = new ItemRadioactive("cake.yellow", ConfigRadiation.RADIOACTIVE_MAT_VALUE_YELLOW_CAKE).setRegistryName(AtomicScience.PREFIX + "yellow_cake"));
-        event.getRegistry().register(itemUranium235 = new ItemRadioactive("uranium.235",  ConfigRadiation.RADIOACTIVE_MAT_VALUE_U235).setRegistryName(AtomicScience.PREFIX + "uranium_235"));
+        event.getRegistry().register(itemUranium235 = new ItemRadioactive("uranium.235", ConfigRadiation.RADIOACTIVE_MAT_VALUE_U235).setRegistryName(AtomicScience.PREFIX + "uranium_235"));
         event.getRegistry().register(itemUranium238 = new ItemRadioactive("uranium.238", ConfigRadiation.RADIOACTIVE_MAT_VALUE_U238).setRegistryName(AtomicScience.PREFIX + "uranium_238"));
 
         //Waste items
         event.getRegistry().register(itemProcessingWaste = new ItemRadioactive("processing.waste", ConfigRadiation.RADIOACTIVE_MAT_VALUE_YELLOW_CAKE).setRegistryName(AtomicScience.PREFIX + "processing_waste"));
-        event.getRegistry().register(itemToxicWaste = new ItemRadioactive("toxic.waste",  ConfigRadiation.RADIOACTIVE_MAT_VALUE_YELLOW_CAKE).setRegistryName(AtomicScience.PREFIX + "toxic_waste"));
+        event.getRegistry().register(itemToxicWaste = new ItemRadioactive("toxic.waste", ConfigRadiation.RADIOACTIVE_MAT_VALUE_YELLOW_CAKE).setRegistryName(AtomicScience.PREFIX + "toxic_waste"));
 
         //Tools
         event.getRegistry().register(itemHeatProbe = new ItemHeatProbe());
         event.getRegistry().register(itemWrench = new ItemWrench());
 
+        //Register item blocks
+        blockReactorCell = addItemBlock(event.getRegistry(), ASBlocks.blockReactorCell);
+        blockReactorController = addItemBlock(event.getRegistry(), ASBlocks.blockReactorController);
+        blockSteamFunnel = addItemBlock(event.getRegistry(), ASBlocks.blockSteamFunnel);
+        blockSteamTurbine = addItemBlock(event.getRegistry(), ASBlocks.blockSteamTurbine);
+
+        blockUraniumOre = addItemBlock(event.getRegistry(), ASBlocks.blockUraniumOre);
+
+        blockChemExtractor = addItemBlock(event.getRegistry(), ASBlocks.blockChemExtractor);
+        blockChemBoiler = addItemBlock(event.getRegistry(), ASBlocks.blockChemBoiler);
+        blockChemCentrifuge = addItemBlock(event.getRegistry(), ASBlocks.blockChemCentrifuge);
+
         if (AtomicScience.runningAsDev)
         {
             new CreativeTabCells();
         }
+    }
+
+    static ItemBlock addItemBlock(IForgeRegistry<Item> reg, Block block)
+    {
+        ItemBlock item = new ItemBlock(block);
+        item.setRegistryName(block.getRegistryName());
+        reg.register(item);
+        return item;
     }
 }
