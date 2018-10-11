b69efb02e0965c36a4f8c7a73c5820f9cc248dcb
Robert WC Seifert <rseifert.phone@gmail.com>
2018-10-11 11:07:58 -0400

    Added a note why IRadioactiveItem is depricated

--- src/main/java/com/builtbroken/atomic/api/radiation/IRadioactiveItem.java
+++ src/main/java/com/builtbroken/atomic/api/radiation/IRadioactiveItem.java
@@ -10,7 +10,7 @@ import net.minecraft.item.ItemStack;
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 5/8/2018.
  */
-@Deprecated
+@Deprecated //Being replaced with capability system
 public interface IRadioactiveItem
 {
     /**
