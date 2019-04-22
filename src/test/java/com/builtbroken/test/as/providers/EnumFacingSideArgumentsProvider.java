d060c836eff4e08651abb3009173d9a17bdcc0e2
Robert WC Seifert <rseifert.phone@gmail.com>
2019-04-22 12:21:34 -0400

    Created argument providers for EnumFacing

---
+++ src/test/java/com/builtbroken/test/as/providers/EnumFacingSideArgumentsProvider.java
@@ -0,0 +1,30 @@
+package com.builtbroken.test.as.providers;
+
+import net.minecraft.util.EnumFacing;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Permutations of tubes and rotations
+ * Created by Dark(DarkGuardsman, Robert) on 2019-04-22.
+ */
+public class EnumFacingSideArgumentsProvider implements ArgumentsProvider
+{
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext)
+    {
+        final List<Arguments> list = new ArrayList();
+        for (EnumFacing facing : EnumFacing.HORIZONTALS)
+        {
+            list.add(Arguments.of(facing));
+        }
+
+        return list.stream();
+    }
+}
