32189f00c368b4d7c82115d583c7b6a8ea27c8c6
Robert WC Seifert <rseifert.phone@gmail.com>
2019-07-04 16:47:11 -0400

    Fixed reactor pipe rod creating see through blocks

--- src/main/java/com/builtbroken/atomic/content/machines/pipe/reactor/inv/BlockRodPipeInv.java
+++ src/main/java/com/builtbroken/atomic/content/machines/pipe/reactor/inv/BlockRodPipeInv.java
@@ -8,10 +8,14 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
 
@@ -50,5 +54,40 @@ public class BlockRodPipeInv extends BlockPrefab implements ITileEntityProvider
     public TileEntity createNewTileEntity(World worldIn, int meta)
     {
         return new TileEntityRodPipeInv();
+    }
+
+    //-----------------------------------------------
+    //-------- Properties ---------------------------
+    //----------------------------------------------
+
+    @Override
+    public boolean isBlockNormalCube(IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public EnumBlockRenderType getRenderType(IBlockState state)
+    {
+        return EnumBlockRenderType.MODEL;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getRenderLayer()
+    {
+        return BlockRenderLayer.CUTOUT;
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isNormalCube(IBlockState state)
+    {
+        return false;
     }
 }
--- src/main/java/com/builtbroken/atomic/content/machines/pipe/reactor/pass/BlockRodPipe.java
+++ src/main/java/com/builtbroken/atomic/content/machines/pipe/reactor/pass/BlockRodPipe.java
@@ -7,10 +7,14 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
 
@@ -41,5 +45,40 @@ public class BlockRodPipe extends BlockPrefab implements ITileEntityProvider
     public TileEntity createNewTileEntity(World worldIn, int meta)
     {
         return new TileEntityRodPipe();
+    }
+
+    //-----------------------------------------------
+    //-------- Properties ---------------------------
+    //----------------------------------------------
+
+    @Override
+    public boolean isBlockNormalCube(IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public EnumBlockRenderType getRenderType(IBlockState state)
+    {
+        return EnumBlockRenderType.MODEL;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getRenderLayer()
+    {
+        return BlockRenderLayer.CUTOUT;
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isNormalCube(IBlockState state)
+    {
+        return false;
     }
 }
